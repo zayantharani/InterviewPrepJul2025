2149b6a90e16a70f2c41bd5a4c4817d695529d9e
Zayan Tharani <zayantharani@gmail.com>
2025-07-20 14:56:07 -0500

    Initial Commit

---
+++ InteviewPrep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteviewPrep
+//
+//  Created by Zayan Tharani on 7/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteviewPrep/InteviewPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteviewPrepApp.swift
+//  InteviewPrep
+//
+//  Created by Zayan Tharani on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct InteviewPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
